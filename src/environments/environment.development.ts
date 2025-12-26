176cf1cdddee5e95fd3e3b0a4c38be85fcd414dd
ManuPunk16 <luisadrianhdzsoto@gmail.com>
2025-12-26 11:24:27 -0600

    feat: agregar archivos de entorno para desarrollo y producción

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://kash-flow-pos.vercel.app/api',
+  firebase: {
+    apiKey: 'AIzaSyDz1vdzZg-AGm8Jm0psWf6Wap_VdQHDwis',
+    authDomain: 'kash-flow-pos.firebaseapp.com',
+    projectId: 'kash-flow-pos',
+    storageBucket: 'kash-flow-pos.firebasestorage.app',
+    messagingSenderId: '116201574596',
+    appId: '1:116201574596:web:380b3dfbbddd2054e8b991',
+  },
+};
